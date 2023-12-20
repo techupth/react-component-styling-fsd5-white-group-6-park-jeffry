b4e17d0d875d1b860d72840ad2731ad289b926c5
Astrick <mypiarenoo@gmail.com>
2023-12-20 15:08:07 +0700

    (Style): Button and alert to react syntax

--- src/components/Alert.jsx
+++ src/components/Alert.jsx
@@ -1,3 +1,132 @@
-// Start coding here
+/** @jsxImportSource @emotion/react */
+import { css } from "@emotion/react";
+import frownImage from "../images/frown.svg";
+import alerttriangle from "../images/alert-triangle.svg";
+import alertcircle from "../images/check-circle.svg";
+import checkcircle from "../images/check-circle.svg";
 
-function Alert() {}
+function Alert(props) {
+  if (props.type === "error") {
+    return (
+      <div
+        css={css`
+          width: 40.625rem;
+          height: 4.75rem;
+          border-radius: 0.625rem;
+          background: #f9c8c8;
+          display: flex;
+          align-items: center;
+          margin: 1.9rem 0rem 1.9rem 0rem;
+        `}
+      >
+        <img
+          src={frownImage}
+          alt="error icon"
+          css={css`
+            margin-left: 1.5rem;
+            margin-right: 1.2rem;
+          `}
+        />
+        <h2
+          css={css`
+            color: #444444;
+          `}
+        >
+          This is an error alert box
+        </h2>
+      </div>
+    );
+  } else if (props.type === "warning") {
+    return (
+      <div
+        css={css`
+          width: 40.625rem;
+          height: 4.75rem;
+          border-radius: 0.625rem;
+          background: #f9d9c8;
+          display: flex;
+          align-items: center;
+          margin: 1.9rem 0rem 1.9rem 0rem;
+        `}
+      >
+        <img
+          src={alerttriangle}
+          alt="warning icon"
+          css={css`
+            margin-left: 1.5rem;
+            margin-right: 1.2rem;
+          `}
+        />
+        <h2
+          css={css`
+            color: #444444;
+          `}
+        >
+          This is an warning alert box
+        </h2>
+      </div>
+    );
+  } else if (props.type === "info") {
+    return (
+      <div
+        css={css`
+          width: 40.625rem;
+          height: 4.75rem;
+          border-radius: 0.625rem;
+          background: #f9ebc8;
+          display: flex;
+          align-items: center;
+          margin: 1.9rem 0rem 1.9rem 0rem;
+        `}
+      >
+        <img
+          src={alertcircle}
+          alt="info icon"
+          css={css`
+            margin-left: 1.5rem;
+            margin-right: 1.2rem;
+          `}
+        />
+        <h2
+          css={css`
+            color: #444444;
+          `}
+        >
+          This is info alert box
+        </h2>
+      </div>
+    );
+  } else if (props.type === "success") {
+    return (
+      <div
+        css={css`
+          width: 40.625rem;
+          height: 4.75rem;
+          border-radius: 0.625rem;
+          background: #cef7cd;
+          display: flex;
+          align-items: center;
+          margin: 1.9rem 0rem 1.9rem 0rem;
+        `}
+      >
+        <img
+          src={checkcircle}
+          alt="success icon"
+          css={css`
+            margin-left: 1.5rem;
+            margin-right: 1.2rem;
+          `}
+        />
+        <h2
+          css={css`
+            color: #444444;
+          `}
+        >
+          This is success alert box
+        </h2>
+      </div>
+    );
+  }
+}
+
+export default Alert;
--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,3 +1,38 @@
-// Start coding here
+/** @jsxImportSource @emotion/react */
+import { css } from "@emotion/react";
 
-function Button() {}
+function Button(props) {
+  if (props.type === "primary") {
+    return (
+      <button
+        css={css`
+          background-color: #074ee8;
+          margin-bottom: 1.7rem;
+          width: 171.19px;
+          height: 50px;
+          border-radius: 4px;
+          font-weight: lighter;
+        `}
+      >
+        Large
+      </button>
+    );
+  } else if (props.type === "secondary") {
+    return (
+      <button
+        css={css`
+          background-color: #07a4e8;
+          margin-bottom: 1.7rem;
+          width: 171.19px;
+          height: 50px;
+          border-radius: 4px;
+          font-weight: lighter;
+        `}
+      >
+        Large
+      </button>
+    );
+  }
+}
+
+export default Button;
