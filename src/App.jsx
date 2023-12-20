09f1c85de8e3216e2fdef8b290b21ff3995cff52
Park <tanagrit.sornchaiyagorn@gmail.com>
2023-12-20 23:55:23 +0700

    style: add style css inline with emotion/react to components files

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,19 @@
 import "./App.css";
-
+import { Button } from "./components/Button";
+import { Alert } from "./components/Alert";
 function App() {
   return (
     <div className="App">
       <div className="button-components-section">
-        {/* Render ตัว Button 2 แบบ */}
+        <Button type="Primary"></Button>
+        <Button type="Secondary"></Button>
       </div>
       <hr />
       <div className="alert-components-section">
-        {/* Render ตัว Alert 4 แบบ */}
+        <Alert index={0} text="This is error alert box"></Alert>
+        <Alert index={1} text="This is waring alert box"></Alert>
+        <Alert index={2} text="This is info alert box"></Alert>
+        <Alert index={3} text="This is succes alert box"></Alert>
       </div>
     </div>
   );
